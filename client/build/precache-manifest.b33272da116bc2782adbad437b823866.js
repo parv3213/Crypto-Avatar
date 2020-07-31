5a77ea7ded7cf13a332eda14bab9a59d085cae20
parv3213 <parv3213@gmail.com>
2020-07-31 13:37:10 +0530

    added built

---
+++ client/build/precache-manifest.b33272da116bc2782adbad437b823866.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68781d7d7d6b5c63515de5f8e4adcc6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa3aede3e305d70bb12d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2ef7b267bc51902decfa",
+    "url": "/static/css/main.d8a1c4f6.chunk.css"
+  },
+  {
+    "revision": "aa3aede3e305d70bb12d",
+    "url": "/static/js/2.d13e15f5.chunk.js"
+  },
+  {
+    "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
+    "url": "/static/js/2.d13e15f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef7b267bc51902decfa",
+    "url": "/static/js/main.657847df.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
